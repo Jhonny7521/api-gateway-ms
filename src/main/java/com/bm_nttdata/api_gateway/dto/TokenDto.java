07f3ae3f65074a20f4360f19d2a7d11a033b3324
Jhonny Quiliche <jhonny.jql1210@gmail.com>
2024-12-10 17:00:59 -0500

    :sparkles: feat: Agrega filtro de autenticacion mediante jwt

---
+++ src/main/java/com/bm_nttdata/api_gateway/dto/TokenDto.java
@@ -0,0 +1,15 @@
+package com.bm_nttdata.api_gateway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class TokenDto {
+
+    private String token;
+}
